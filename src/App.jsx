06bca2264373e0d08da8f225b9d5d7f6bf18e3bd
thiagoserafim18 <thiagoserafim0180@gmail.com>
2024-08-14 16:47:12 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/Newproject';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Costs</h1>
-    </div>
+
+    <Router>
+      <div>
+        <link to="/">Home</link>
+        <link to="/company">Empresa</link>
+        <link to="/contact">Contato</link>
+        <link to="/newproject">Novo Projeto</link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/newproject' element={<NewProject />} />
+      </Routes>
+      <p>footer</p>
+    </Router>
   );
 }
 
