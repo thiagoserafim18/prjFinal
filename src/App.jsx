69bfedf6dea06097b889421d22dbd9f526271d2a
thiagoserafim18 <thiagoserafim0180@gmail.com>
2024-08-16 17:01:51 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/Newproject';
+import Container from './components/layout/Container';
+import Projects from './components/pages/Projects';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
 
     <Router>
-      <div>
-        <link to="/">Home</link>
-        <link to="/company">Empresa</link>
-        <link to="/contact">Contato</link>
-        <link to="/newproject">Novo Projeto</link>
-      </div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/company' element={<Company />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/newproject' element={<NewProject />} />
-      </Routes>
-      <p>footer</p>
+      <Container>
+        <Navbar />
+        <Routes customClass='min-height'>
+          <Route path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+        <p>footer</p>
+      </Container>
     </Router>
   );
 }
